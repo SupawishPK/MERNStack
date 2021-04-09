0a3d2deac31d6c9b81ab0183702fb84ff6040ed9
supawish34 <supawish.kj@gmail.com>
2021-04-09 10:27:01 +0700

    Create Register & login Forms

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,48 @@
+import BgImage from './BgImage'
+
 const Login = () => {
   return (
-    <div>
-      <h1 style={{ marginTop: "100px" }}>Login</h1>
-    </div>
+    <>
+      <div className="row mt-80">
+        <div className="col-8">
+          <BgImage />
+        </div>
+        <div className="col-4">
+          <div className="account">
+            <div className="account__section">
+              <form>
+                <div className="group">
+                  <h3 className="form-heading">Login</h3>
+                </div>
+                <div className="group">
+                  <input
+                    type="email"
+                    name=""
+                    className="group__control"
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div className="group">
+                  <input
+                    type="password"
+                    name=""
+                    className="group__control"
+                    placeholder="Create Password"
+                  />
+                </div>
+                <div className="group">
+                  <input
+                    type="submit"
+                    className="btn btn-default btn-block"
+                    value="Login"
+                  />
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
