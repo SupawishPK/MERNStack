06b2768f80d084b020041b9e05c11e12f65321a5
supawish34 <supawish.kj@gmail.com>
2021-04-09 10:56:15 +0700

    SEO Friendly Components

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,10 +1,16 @@
-
+import { Helmet } from "react-helmet";
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    )
-}
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Learn HTML, CSS, JavaScript, React, Vue, Flutter etc"
+        />
+        <title>Web articles</title>
+      </Helmet>
+    </>
+  );
+};
 
 export default Home;
