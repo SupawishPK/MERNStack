b8643c274519896479730862ee59da1789500aa5
supawish34 <supawish.kj@gmail.com>
2021-04-09 08:43:50 +0700

    Create Navbar Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import "./main.scss";
 import Home from "./components/Home";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import Navbar from './components/Navbar'
 function App() {
   return (
     <Router>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
